b3b89fd26902d343a43f14db7e5641f8a99057f1
Phil <pgoelter>
2020-12-07 15:57:02 +0000

    Initial commit

---
+++ src/test/java/sar/microservices/PaymentServiceTest.java
@@ -0,0 +1,21 @@
+package sar.microservices;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class PaymentServiceTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
